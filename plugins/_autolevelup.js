fc4fcea2364e8022fe26a0edbd1fc6e7c523d606
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-07 06:26:34 +0700

    Update _autolevelup.js
    .

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -10,7 +10,7 @@ handler.before = m => {
         let str = `Selamat @${m.sender.split`@`[0]} Kamu Telah Naik level 😱
 *Level Sebelumnya : ${before}*\nLevel Saat Ini : *${user.level}*
 `.trim()
-        conn.send2Button(m.chat, str, footer, 'PROFILE', '#profile', 'Off AutoLevelUp', '#off autolevelup', false, {
+        conn.send2Button(m.chat, str, footer, 'PROFILE', '#profile', 'MENU', '#menu', false, {
             contextInfo: {
                 mentionedJid: [m.sender]
             }
