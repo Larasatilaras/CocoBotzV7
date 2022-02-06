971e16268e12f3ed46794cb1ad4322e945586aea
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-06 18:13:29 +0700

    Update hks.js
    .

--- plugins/hks.js
+++ plugins/hks.js
@@ -8,28 +8,25 @@ let handler = async (m, { conn }) => {
     let uptime = clockString(_uptime)
 
     m.reply(`
-┏━━━ꕥ〔 *Status* 〕ꕥ━⬣
-┃✾ Aktif selama ${uptime}
-┃✾ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
-┃✾ *${groups.length}* Grup
-┃✾ *${chats.length - groups.length}* Chat Pribadi
-┃✾ *${Object.keys(global.db.data.users).length}* Pengguna
-┃✾ *${totaljadibot.length}* Jadibot
-┃✾ *${conn.blocklist.length}* Terblock
-┃✾ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-┃✾ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-┗━ꕥ
+〔 *Status* 〕
+Aktif selama ${uptime}
+Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+*${groups.length}* Grup
+*${chats.length - groups.length}* Chat Pribadi
+*${Object.keys(global.db.data.users).length}* Pengguna
+*${totaljadibot.length}* Jadibot
+*${conn.blocklist.length}* Terblock
+*${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+*${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 
-┏━━━ꕥ〔 *Pengaturan* 〕ꕥ━⬣
-┃✾ ${anon ? '✅' : '❌'} *Anon Chat*
-┃✾ ${anticall ? '✅' : '❌'} *Anti Call*
-┃✾ ${antispam ? '✅' : '❌'} *Anti Spam*
-┃✾ ${antitroli ? '✅' : '❌'} *Anti Troli*
-┃✾ ${backup ? '✅' : '❌'} *Auto Backup DB*
-┃✾ ${groupOnly ? '✅' : '❌'} *Mode Grup*
-┃✾ ${jadibot ? '✅' : '❌'} *Jadi Bot*
-┃✾ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
-┗━ꕥ
+〔 *Pengaturan* 〕
+${anon ? '✅' : '❌'} *Anon Chat*
+${anticall ? '✅' : '❌'} *Anti Call*
+${antispam ? '✅' : '❌'} *Anti Spam*
+${antitroli ? '✅' : '❌'} *Anti Troli*
+${backup ? '✅' : '❌'} *Auto Backup DB*
+${groupOnly ? '✅' : '❌'} *Mode Grup*
+${jadibot ? '✅' : '❌'} *Jadi Bot*
     `.trim())
 }
 handler.help = ['botstatus']
