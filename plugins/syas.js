66378d9f83c35e5406cc7513ebf38e7d9dc0d6f7
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-06 18:48:19 +0700

    Update syas.js
    .

--- plugins/syas.js
+++ plugins/syas.js
@@ -1,9 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(`https://i.ibb.co/rkTg7B0/donasi.jpg`)).buffer(), `
 ┌〔 𝘿𝙤𝙣𝙖𝙨𝙞 • 𝙀𝙢𝙤𝙣𝙚𝙮 〕
-├ _DANA_ : *0882-7926-8363*
-├ _SAWERIA_ : *https://saweria.co/raraharsita2*
-├ *SELAIN DANA VIA QR AJA ATAU SAWERIA*
+├ _Pulsa_ : *0895-3544-37300*
+├ _Saweria_ : *https://saweria.co/cocopie*
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
