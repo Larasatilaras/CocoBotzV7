6bca338e9208a3dd519e188154b6fd17390368ec
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-27 17:53:12 +0700

    Update _antilink.js
    .

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,18 +1,20 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.db.data.chats[m.chat]
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
-    let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
-    if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-    await conn.sendButton(m.chat, `*Link Grup Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *#on restrict* supaya bisa kick'}`, footer, 'Matikan Antilink', '#off antilink', m)
-    //if (global.opts['restrict']) {
-      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
-//    }
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK DETECTOR 」*\n\nTerdeteksi *${await this.getName(m.sender)}* Telah Mengirim Link Group Wangsaff\n\nMaaf Kamu Akan Di Kick Dari Grup Ini Byee! 👋🗿`)
+    if (isAdmin) return m.reply('*Eh Maaf Kamu Admin, Hehe :3...*')
+    if (!isBotAdmin) return m.reply('*Bot Bukan Admin, Mana Bisa Kick Orang _-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol Ngirim Link Group Sendiri :v*')
+    await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
